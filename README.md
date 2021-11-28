015714c0ba51d006bd31f7a6d7fa13318ddb72ee
gregalvessatorres <greg.satorres@before.com.br>
2021-11-28 14:33:59 -0300

    Aplicados novos ajustes de inicialização e read.me

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Project for studing lumen
+# Project to study Lumen
 
 ## Instructions
 
-- tbd
+### Requirements
+[Docekr](https://docs.docker.com/engine/install/ubuntu/).
+
+[Docekr Compose](https://docs.docker.com/compose/install/).
+
+### Instructions
+After cloning the project
+
+For the first time execution run the file "init.sh".
+
+    bash init.sh
+
+All other executions just run the file "run.sh".
+
+    bash run.sh
---
+++ init.sh
@@ -0,0 +1,12 @@
+echo 'Iniciando:'
+docker-compose up --build -d
+echo 'Dump autload'
+docker exec -it php-transfer composer dump-autoload
+docker exec -it php-transfer composer install
+echo 'Copia do .env'
+cd transfer && cp .env.example .env
+echo 'Executar Migrations'
+docker exec -it php-transfer php artisan migrate
+docker exec -it php-transfer php artisan db:seed
+echo 'Iniciando fila de notificações'
+docker exec -it php-transfer php artisan queue:work
