1c211194cc8552c1c3437572366474c8db325dd7
gregalvessatorres <greg.satorres@before.com.br>
2021-11-28 13:58:51 -0300

    Ajustado arquivo de incialização

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
-docker ps -a
+echo 'Iniciando:'
+docker-compose up -d
+echo 'Dump autload'
+docker exec -it php-transfer composer dump-autoload
+docker exec -it php-transfer composer install
+echo 'Copia do .env'
+cd transfer && cp .env.example .env
+echo 'Executar Migrations'
+docker exec -it php-transfer php artisan migrate
+docker exec -it php-transfer php artisan db:seed
+echo 'Iniciando fila de notificações'
+docker exec -it php-transfer php artisan quere:work
